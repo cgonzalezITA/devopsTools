346f9b29ad84ee386d6c0aedfa02303245f2e534
cgonzalez <cgonzalez@V25147>
2025-10-03 12:46:08 +0200

    pEnvironment extended with conda as package manager

--- pTools/pEnvironment.sh
+++ pTools/pEnvironment.sh
@@ -14,7 +14,7 @@
 SCRIPTNAME=$BASH_SOURCE
 if [ "$0" == "$BASH_SOURCE" ]; then CALLMODE="executed"; else CALLMODE="sourced"; fi
 BASEDIR=$(dirname "$SCRIPTNAME")
-
+PACKAGEMANAGER=pip
 SHOWHELP=false
 # Creates the env for the project
 ENVROOTFOLDER=/python/.envs # This is just a proposal, feel free to change it
@@ -40,6 +40,8 @@ function help() {
     # \t-f <folder with artifacts>: Folder where the artifact file must be located (def value: ./KArtifacts \n
     HELP="$HELP\nHELP: USAGE: $SCRIPTNAME [optArgs] \n 
             \t[-p | --pythoncmd] <pythoncmd>: Python command to use (def. python or python3 if python not found) \n
+            \t[-pm | --packageManager] <packageManager>: Python's package manager (def. pip). Values [pip|conda]\n
+            \texport DEF_PTOOLS_PACKAGEMANAGER=<packageManager>: To avoid using -pm param, you can set this env var before running the script \n              
             \t[-rf | --rootfolder] <rootfolder>: Root folder where the envs are stored (def. $ENVROOTFOLDER) \n
             \t[-e | --envname] <envname>: Name of the python env (def. $ENVNAME) \n
             \texport DEF_PTOOLS_ENVNAME=<env_name>: To avoid using -e param, you can set this env var before running the script \n              
@@ -62,15 +64,20 @@ while true; do
     [[ "$#" -eq 0 ]] && break;
     case "$1" in
         -h | --help ) 
+        
             SHOWHELP=true;
             break ;;
         -lenvs | --listenvs )
             LISTENVS=true; shift ;;
-        # \t-c | --createenv: Creates python env (def.false)\n
+        # \t-pm | --packageManager: Python's package manager (def. pip). Values [pip|conda] \n
+        -pm | --packageManager )
+            if [[ "$#" -le 1 ]]; then echo "Error: Missing argument for -pm|--packageManager option"; [ "$CALLMODE" == "executed" ] && exit -1 || return -1; fi
+            PACKAGEMANAGER=$2; shift 2 ;;
         # \t-p | --pythoncmd: Python command to use (def. python or python3 if python not found) \n
         -p | --pythoncmd )
             if [[ "$#" -le 1 ]]; then echo "Error: Missing argument for -p|--pythoncmd option"; [ "$CALLMODE" == "executed" ] && exit -1 || return -1; fi
             PYTHONCMD=$2; shift 2 ;;
+        # \t-c | --createenv: Creates python env (def.false)\n
         -c | --createenv )
             CREATE=true; shift ;;
         # \t-a | --activate: Activates python env (def. true) \n
@@ -126,7 +133,13 @@ if [ "$CUSTOM_ENVNAME" = false ] && [ -n "$DEF_PTOOLS_ENVNAME" ]; then
     ENVNAME=$DEF_PTOOLS_ENVNAME
     ENVNAMECOMESFROMENV=true
 fi
-echo "# Working with python version: $PVERSION ($PYTHONCMD)."
+PMANAGER_COMESFROMENV=false
+if [ -n "$DEF_PTOOLS_PACKAGEMANAGER" ]; then
+    PACKAGEMANAGER=$DEF_PTOOLS_PACKAGEMANAGER
+    PMANAGER_COMESFROMENV=true
+fi
+echo "# Working with python version: $PVERSION ($PYTHONCMD)." 
+echo "# Package manager: $PACKAGEMANAGER $([ "$PMANAGER_COMESFROMENV" = true ] && echo " (It comes from DEF_PTOOLS_PACKAGEMANAGER env var)")"
 echo "# ENVROOTFOLDER=[$ENVROOTFOLDER]"
 echo "# ENVNAME=[$ENVNAME] $([ "$ENVNAMECOMESFROMENV" = true ] && echo " (It comes from DEF_PTOOLS_ENVNAME env var)")"
 PYTHONENV=$ENVROOTFOLDER/$ENVNAME/bin  
@@ -141,31 +154,66 @@ if [ "$LISTENVS" = true ]; then
     [ "$CALLMODE" == "executed" ] && exit -1 || return -1;
 fi
 if [ "$CREATE" = true ]; then
-    echo "# Creating python env $ENVNAME int $ENVROOTFOLDER..."
-    read -p "Press [ENTER] to continue or [CTRL+C] to abort"
+    echo "# Creating python env $ENVNAME in $ENVROOTFOLDER..."
     sudo mkdir -p  $ENVROOTFOLDER
-    $PYTHONCMD -m venv --copies $ENVROOTFOLDER/$ENVNAME 
+    if [ -d "$ENVROOTFOLDER/$ENVNAME" ]; then
+        MSG="WARNING: $ENVROOTFOLDER/$ENVNAME already exists. Do you want to overwrite it? (y/n)"
+        read -p "$MSG " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            rm -rf $ENVROOTFOLDER/$ENVNAME
+        fi
+    fi
+    if [ ! -d "$ENVROOTFOLDER/$ENVNAME" ]; then
+        if [ $PACKAGEMANAGER == "pip" ]; then
+            CMD="$PYTHONCMD -m venv --copies $ENVROOTFOLDER/$ENVNAME"
+        else
+            CMD="$PACKAGEMANAGER create -y -p $ENVROOTFOLDER/$ENVNAME"
+        fi
+    echo "# Running command: [$CMD]"
+    read -p "Press [ENTER] to continue or [CTRL+C] to abort"
+    $CMD        
+    fi
 fi
 
 if [ "$ACTIVATE" = true ]; then
-    echo "# Activating python env $PYTHONENV..."
+    echo "# Activating python env $ENVROOTFOLDER/$ENVNAME..."
+    if [ $PACKAGEMANAGER == "pip" ]; then
+        CMD="source $PYTHONENV/activate";
+    else
+        CMD="$PACKAGEMANAGER activate $ENVROOTFOLDER/$ENVNAME"
+    fi
+    echo "# Running command: [$CMD]"
     read -p "Press [ENTER] to continue or [CTRL+C] to abort"
-    source $PYTHONENV/activate  
+    $CMD        
 fi
 if [ -n "$REQUIREMENTS_FILE" ]; then
     echo "# Installing requirements from $REQUIREMENTS_FILE..."
     read -p "Press [ENTER] to continue or [CTRL+C] to abort"
-    pip install --no-cache-dir -r $REQUIREMENTS_FILE
+    if [ $PACKAGEMANAGER == "pip" ]; then
+        $PACKAGEMANAGER install --no-cache-dir -r $REQUIREMENTS_FILE
+    else
+        $PACKAGEMANAGER install --file $REQUIREMENTS_FILE
+    fi
 fi
 
 if [ "$EXPORT_REQUIREMENTS" = true ]; then
-    echo "# Running command pip list --not-required --format=freeze to export the list of installed packages..."
-    echo "# pip freeze --all can also be used to include all dependencies"
-    pip list --not-required --format=freeze
+    if [ $PACKAGEMANAGER == "pip" ]; then
+        echo "# Running command pip list --not-required --format=freeze to export the list of installed packages..."
+        echo "# pip freeze --all can also be used to include all dependencies"
+        $PACKAGEMANAGER list --not-required --format=freeze
+    else
+        echo "# Running command conda list --explicit --md5 to export the list of installed packages..."
+        $PACKAGEMANAGER list --explicit --md5
+    fi
     [ "$CALLMODE" == "executed" ] && exit -1 || return -1;
 fi
 
 if [ "$DEACTIVATE" = true ]; then
     echo "Deactivating python env $PYTHONENV..."
-    deactivate
+    if [ $PACKAGEMANAGER == "pip" ]; then
+        deactivate
+    else
+        $PACKAGEMANAGER deactivate
+    fi
 fi
