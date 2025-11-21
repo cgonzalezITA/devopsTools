87df05b73f4d0740b9ef17721b452344c177a060
cgonzalez <cgonzalez@V25004>
2025-11-21 19:15:11 +0100

    dGet reviewed and some minor issues fixed

--- dTools/dGet.sh
+++ dTools/dGet.sh
@@ -20,7 +20,7 @@ BASEDIR=$(dirname "$SCRIPTNAME")
 VERBOSE=true
 # FOLDER_ARTIFACTS=./KArtifacts/
 COMMAND=get
-ARTIFACT=ps
+ARTIFACT=""
 CCLUE=""
 # \t-fs: Force secretname match the given clue (using this, the clue is not a clue, but the name)       \n
 USECCLUE=true
@@ -33,12 +33,11 @@ PROJECTNAME=""
 #############################
 function help() {
     # \t-f <folder with artifacts>: Folder where the artifact file must be located (def value: ./KArtifacts \n
-    HELP="HELP: USAGE: $SCRIPTNAME [optArgs] [<component clue>] \n 
+    HELP="HELP: USAGE: $SCRIPTNAME [optArgs] [<docker artifact>] [<component clue>] \n 
             \t-h: Show help info                                                                                       \n
             \t-fv: Force value match the given clue (using this, the clue is not a clue, but the name)                 \n
             \t-v: Do not show verbose info                                                                             \n
             \t-p: Show components of project (These components were created by a docker-component cmd with a project)  \n
-            \t[<component clue>]: Clue to identify the artifact file name|all                                          \n
             \t[<docker artifact>]: docker Artifact to show info about. Values: ps*, all, network, ...                  \n
             \n[<component clue>] and [<docker artifact>] can in some context be swapped to match existing artifacts"
     if test "$#" -ge 1; then
@@ -69,44 +68,49 @@ while true; do
             if [[ $1 == -* ]]; then
                 echo -e $(help "ERROR: Unknown parameter [$1]");
                 [ "$CALLMODE" == "executed" ] && exit -1 || return -1;
+            elif test "${#ARTIFACT}" -eq 0; then
+                ARTIFACT=$1;
+            elif test "${#CCLUE}" -eq 0; then
+                CCLUE=$1;
             fi ;
             shift ;;
     esac
 done
 
 PROVIDEDPARAMS=$#
-if test "$#" -ge 1; then
-    ARTIFACT=$1; shift;
+if test "${#ARTIFACT}" -eq 0; then
+    ARTIFACT=ps;
 fi
-if test "$#" -lt 1; then
-    USECCLUE=false
-    CCLUE="all"
-else
-    CCLUE=$1
-    shift;
-fi
+
+# echo "Artifact selected: [$ARTIFACT]"
+
 if [[ ! ${ARTIFACTSFULLNAMES[@]} =~ " $ARTIFACT " ]]
 then
-# echo "Artifact value not found: $PROVIDEDPARAMS"
+# echo "Artifact $ARTIFACT not found: $ARTIFACTSFULLNAMES"
 # Swapping is done between ARTIFACT AND CCLUE
-    if test "$PROVIDEDPARAMS" -le 1; then
-        CCLUE=$ARTIFACT
-        ARTIFACT=ps
-    elif test "$PROVIDEDPARAMS" -le 2; then
+    TMP=$CCLUE
+    CCLUE=$ARTIFACT
+    ARTIFACT=$TMP
+    if test "${#ARTIFACT}" -eq 0; then
+        ARTIFACT=ps;
+    fi
+    if [[ ! ${ARTIFACTSFULLNAMES[@]} =~ " $ARTIFACT " ]]
+    then
         TMP=$CCLUE
         CCLUE=$ARTIFACT
         ARTIFACT=$TMP
+        echo -e $(help "ERROR: Unknown artifact [$ARTIFACT]");
+        [ "$CALLMODE" == "executed" ] && exit -1 || return -1;
     fi
 fi
-
 if  [ "$ARTIFACT" == "all" ]; then
     # Gets all k8s artifacts. As shown in https://kubernetes.io/docs/concepts/workloads/controllers/deployment/
-    echo "---"
-    echo "Gets artifacts matching the clue [$CCLUE]..."
+    echo "---" > /dev/tty;
+    echo "Gets artifacts matching the clue [$CCLUE]..." > /dev/tty;
     for artifact in $ARTIFACTS; do
         item=$CCLUE
 
-        echo "---"
+        echo "---"> /dev/tty;
         read -p "  Getting [$artifact] $item: (press a key to continue)" -n 1 -r
         echo "";
         $SCRIPTNAME -fv $artifact $item 
@@ -114,7 +118,7 @@ if  [ "$ARTIFACT" == "all" ]; then
     echo ""
     [ "$CALLMODE" == "executed" ] && exit -1 || return -1;
 fi
-if  [ "$CCLUE" == "all" ]; then
+if test "${#CCLUE}" -eq 0; then
     GREPCMD=""
     PATTERNDESC=""
 else
