ff01634bc4135c9c52e4c610d857474451003dc8
cgonzalez <cgonzalez@V24067>
2026-09-15 11:43:47 +0200

    Enhance gCommit.sh to support multi-line commit messages and improve help documentation

--- gTools/gCommit.sh
+++ gTools/gCommit.sh
@@ -36,12 +36,13 @@ function help() {
         HELP="${1}\n"     
     fi
     # \t-f <folder with artifacts>: Folder where the artifact file must be located (def value: ./KArtifacts \n
-    HELP="$HELP\nHELP: USAGE: $SCRIPTNAME [-h] [-s <submodulePath>] <comment1 (with quotes \")> [<comment2 (with quotes \")>] [-t TAG] [-tc TAGCOMMENT] \n 
+    HELP="$HELP\nHELP: USAGE: $SCRIPTNAME [-h] [-s <submodulePath>] <comment1 (with quotes \")> [<comment2 (with quotes \")>] [-t TAG] [-tc TAGCOMMENT] \n
             \t-h: Show help info \n
             \t-y: No confirmation questions are asked \n
             \t[-t|--tag] TAG: Adds a tag to commit \n
             \t[-f| -ft |--forcetag] TAG: Forces an existing tag to be updated to the committed head \n
-            \t[-tc|--tagComment] TAGCOMMENT: Adds a comment to the tag\n"
+            \t[-tc|--tagComment] TAGCOMMENT: Adds a comment to the tag\n
+            \tMulti-line commit via heredoc: $SCRIPTNAME << 'EOF'\n\t  subject line\n\t  \n\t  body paragraph\n\tEOF\n"
     echo $HELP
     HELP="HELP: USAGE: $SCRIPTNAME "
 }
@@ -78,14 +79,25 @@ while true; do
              fi
             shift ; shift ;;
         * ) 
-            if [[ $1 == -* ]]; then
+            if [[ $1 =~ ^-[a-zA-Z0-9-] ]]; then
                 echo -e "WARNING: Unknown parameter [$1]";
             elif test "${#C1}" -eq 0; then
-                C1=$1;
-                C="-m \"$C1\""
+                if [[ "$1" == *$'\n'* ]]; then
+                    C1="\$(cat << 'EOF' 
+$1
+EOF)"
+                else
+                    C1="$1"
+                fi
+                
             elif test "${#C2}" -eq 0; then
-                C2=$1;
-                C="$C -m \"${C2}\""
+                if [[ "$1" == *$'\n'* ]]; then
+                    C2="\$(cat << 'EOF' 
+$1
+EOF)"
+                else
+                    C2="$1"
+                fi
             fi
             shift ;;
     esac
@@ -100,6 +112,7 @@ if test "${#TAG}" -gt 0; then
     test "${#TAGC}" -eq 0 && TAGC=$C1;
 fi
 if [ "$VERBOSE" = true ]; then
+    echo "---"
     echo " PATH= $( pwd )"
     echo " SUBMODULE= [$SUBMODULE]"
     echo " C1= [$C1]"
@@ -107,7 +120,10 @@ if [ "$VERBOSE" = true ]; then
     echo " TAG= [$TAG]"
     echo " TAGCOMMENT=[$TAGC]"
 fi
-CMD="git commit $C"
+COMMIT_ARGS=("-m" "$C1")
+test "${#C2}" -gt 0 && COMMIT_ARGS+=("-m" "$C2")
+CMD="git commit -m \"$C1\""
+test "${#C2}" -gt 0 && CMD="$CMD -m \"$C2\""
 if test "${#TAG}" -gt 0; then
     CMD="$CMD; git tag $TAG -m \"$TAGC\""
 fi
@@ -118,7 +134,7 @@ fi
 if [ "$ASK" = true ]; then
     MSG=$(echo "QUESTION: Are you sure to run the previous command to commit the git changes [Y/n]?" \
                 | sed "s/\(to commit the git changes\)/\x1b[31m\1\x1b[0m/g")
-    read -p "$MSG" -n 1 -r
+    read -p "$MSG" -n 1 -r < /dev/tty
     echo    # (optional) move to a new line
 else
     REPLY="y"
@@ -129,6 +145,9 @@ then
         cd $SUBMODULE;
     fi
     echo ---
-    bash -c "$CMD"
+    git commit "${COMMIT_ARGS[@]}"
+    if test "${#TAG}" -gt 0; then
+        eval "git tag $TAG -m \"$TAGC\""
+    fi
     cd $PWD1
 fi
